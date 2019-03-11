776887dfcae9f912b5ccc1d307b126293480629f
Brenda Barbosa <brenda.barbosa@webjump.com.br>
2019-03-11 11:48:12 -0300

    somaTest.php modificado

--- test/somaTest.php
+++ test/somaTest.php
@@ -1,11 +1,13 @@
 <?php
   use PHPUnit\Framework\TestCase;
 
+   include 'soma.php';
+
   class SomaTest extends TestCase
   {
      public function testSoma ()
      {
-         $this->assertEquals(4, soma(2+2));
+         $this->assertEquals(4, soma(2, 2));
      }
   }
 
